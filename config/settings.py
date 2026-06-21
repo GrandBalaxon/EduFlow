0b633245aeabc2ab8ebeff3ff1df417a2db51cfe
Vlad Spinul <shinokakaov@mail.ru>
2026-06-21 18:06:34 +0300

    feat: created core app + new settings for new apps in settings.py

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
+    "users",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -86,4 +88,12 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
+AUTH_USER_MODEL = "users.User"
