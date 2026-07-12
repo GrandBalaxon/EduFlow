aa352f8f002b861f57dd6d4918916b6ddbab80ba
Vlad Spinul <shinokakaov@mail.ru>
2026-07-12 18:51:21 +0300

    fix: added PermissionsMixin to User model to add missing dependencies

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
@@ -24,7 +25,7 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, verbose_name="Почта", help_text="Введите почту")
     phone_number = models.CharField(
         max_length=20,
@@ -43,7 +44,6 @@ class User(AbstractBaseUser):
     avatar = models.ImageField(upload_to="users/avatar", blank=True, null=True, verbose_name="Аватар профиля")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
