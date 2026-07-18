d9d992585812dd593542052405904d1af80e185f
Vlad Spinul <shinokakaov@mail.ru>
2026-07-18 18:39:37 +0300

    feat: added YT link validation and used it in Lesson serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
 from core.models import Course, Lesson
+from core.validators import YouTubeLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [YouTubeLinkValidator(field='video_link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
