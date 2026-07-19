33fb4d3aa4d92b84505d30a5f7fde5b600a95268
Vlad Spinul <shinokakaov@mail.ru>
2026-07-19 16:33:34 +0300

    feat: added subscription Api view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from users.models import User, Payment
+from core.models import Course
+from users.models import User, Payment, Subscription
 from users.permissions import IsProfileOwner
 from users.serializers import UserPublicSerializer, UserPrivateSerializer, PaymentSerializer
 
@@ -47,3 +51,27 @@ class PaymentsListApiView(generics.ListAPIView):
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
+
+
+class SubscriptionApiView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        course_id = self.request.data.get('course_id')
+        course_item = get_object_or_404(Course, pk=course_id)
+
+        subs_item, created = Subscription.objects.get_or_create(
+            user=user,
+            course=course_item,
+            defaults={'is_active': True}
+        )
+
+        if not created:
+            subs_item.is_active = not subs_item.is_active
+            subs_item.save()
+            message = 'Подписка возобновлена' if subs_item.is_active else 'Подписка отключена'
+        else:
+            message = 'Подписка добавлена'
+
+        return Response({"message": message, "is_active": subs_item.is_active})
